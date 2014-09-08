29002ae2918325ffd57479a4f65de4059f4bd0f4
SakataHirofumi <sumlibe@gmail.com>
2014-09-08 14:33:11 +0900

    #17 ブラウザ→（WebAPI）→FrontEnd→（WebAPI）→BackEndのテストAPIを作成する

--- mysite/webapi/fuel/app/classes/controller/memo.php
+++ mysite/webapi/fuel/app/classes/controller/memo.php
@@ -10,6 +10,39 @@ class Controller_Memo extends Controller_Rest
 		$this->response($memos);
 	}
 
+	public function get_read_backend()
+	{
+
+        // APIのトークン
+        $token = '';
+
+        // Request_Curlを生成
+        // http://fuelphp.jp/docs/1.7/classes/request/curl.html
+        $curl = \Request::forge('http://192.168.10.11/memo/read', 'curl');
+
+        // HTTPメソッドを指定
+        $curl->set_method('get');
+
+        // パラメータを設定
+        $curl->set_params(array('token' => $token));
+
+        // 実行
+        // $responseには、Responseインスタンスが入る
+        $response = $curl->execute()->response();
+
+        // レスポンスコードチェック
+        if ($response->status == 200)
+        {
+            // Formatクラスを利用して、JSONからPHPの配列に変換
+            // http://fuelphp.com/docs/classes/format.html
+            $data = \Format::forge($response->body,'json')->to_array();
+
+            // 中身をチェック
+            // var_dump($data);
+			$this->response($data);
+        }
+	}
+
 	public function post_write()
 	{
 		$memo = Input::post('memo');
--- mysite/webapi/fuel/app/config/routes.php
+++ mysite/webapi/fuel/app/config/routes.php
@@ -3,7 +3,8 @@ return array(
 	'_root_'  => 'welcome/index',  // The default route
 	'_404_'   => 'welcome/404',    // The main 404 route
 	
-        'memo/read' => 'memo/read',
-        'memo/write' => 'memo/write',
+	'memo/read' => 'memo/read',
+	'memo/read_backend' => 'memo/read_backend',
+	'memo/write' => 'memo/write',
 	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
 );
